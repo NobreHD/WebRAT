3ce5619301b315896b5f2799ad6800f35e002a31
NobreHD <leo.nobre.hd@gmail.com>
2020-09-25 11:11:12 +0100

    Troca da matriz de botões por uma array de classes

--- static/button.js
+++ static/button.js
@@ -1,24 +1,31 @@
-const options = [
-    ["Bloquear", "change('!CM rundll32.exe user32.dll,LockWorkStation');", "btn-success"],
-    ["Hibernar", "change('!CM rundll32.exe PowrProf.dll,SetSuspendState');", "btn-info"],
-    ["Encerrar", "change('!CM shutdown -f -s');", "btn-danger"],
-    ["Reiniciar", "change('!CM shutdown -f -r');", "btn-danger"],
-    ["IP Config", "change('!CM ipconfig');", "btn-secondary"],
-    ["WIFI Acessadas", "change('!CM netsh wlan show profiles');", "btn-secondary"],
-    ["Password do WIFI", "getpro('Nome de Rede:','!CM netsh wlan show profile name=?? key=clear', true);", "btn-dark"],
-    ["Lista de Programas Abertos", "change('!CM tasklist');", "btn-secondary"],
-    ["Encerrar Programa", "getpro('PID do Programa:','!CM taskkill /PID ?? /f', false);", "btn-dark"],
-    ["Mostrar Mensagem", "getpro('Mensagem:','!MB ??', false);", "btn-dark"],
-    ["Digitar Mensagem", "getpro('Mensagem:','!TT ??', false);", "btn-dark"],
-    ["Combinação de Teclas", "getpro('Combinação (Separador: +):','!TC ??', false);", "btn-dark"]
-]
-
-function addButton(item, index) {
-    let button = `<div class=\"col col-lg-2 col-6\" style=\"margin: 10px 0\">
-        <button class=\"btn ${item[2]}\" onclick=\"${item[1]}\" style=\"width: 100%; height: 100%\" type=\"button\">${item[0]}</button>
-    </div>`
-    $("#button-stop").append(button)
+class Button {
+    constructor(Text, Action, Style) {
+        this.text = Text;
+        this.action = Action;
+        this.style = Style
+    }
+    string(){
+        return `<div class=\"col col-lg-2 col-6 div-button\">
+            <button class=\"btn ${this.style}\" onclick=\"${this.action}\" type=\"button\">${this.text}</button>
+        </div>`
+    }
 }
+var buttons = [];
+
+buttons.push(new Button("Bloquear", "change('!CM rundll32.exe user32.dll,LockWorkStation');", "btn-success"));
+buttons.push(new Button("Hibernar", "change('!CM rundll32.exe PowrProf.dll,SetSuspendState');", "btn-info"));
+buttons.push(new Button("Encerrar", "change('!CM shutdown -f -s');", "btn-danger"));
+buttons.push(new Button("Reiniciar", "change('!CM shutdown -f -r');", "btn-danger"));
+buttons.push(new Button("IP Config", "change('!CM ipconfig');", "btn-secondary"));
+buttons.push(new Button("WIFI Acessadas", "change('!CM netsh wlan show profiles');", "btn-secondary"));
+buttons.push(new Button("Password do WIFI", "getpro('Nome de Rede:','!CM netsh wlan show profile name=?? key=clear', true);", "btn-dark"));
+buttons.push(new Button("Lista de Programas Abertos", "change('!CM tasklist');", "btn-secondary"));
+buttons.push(new Button("Encerrar Programa", "getpro('PID do Programa:','!CM taskkill /PID ?? /f', false);", "btn-dark"));
+buttons.push(new Button("Mostrar Mensagem", "getpro('Mensagem:','!MB ??', false);", "btn-dark"));
+buttons.push(new Button("Digitar Mensagem", "getpro('Mensagem:','!TT ??', false);", "btn-dark"));
+buttons.push(new Button("Combinação de Teclas", "getpro('Combinação (Separador: +):','!TC ??', false);", "btn-dark"));
+
+buttons.forEach(button => $("#button-stop").append(button.string()));
 
 function getpro(question, code, aspas) {
     let text = prompt(question)
@@ -34,5 +41,4 @@ function change(code) {
     $('#cmd').val(code);
 }
 
-options.forEach(addButton)
 
