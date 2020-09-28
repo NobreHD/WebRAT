98db8998e0a9e3c7e85b154d70da5a7228a905f2
NobreHD <leo.nobre.hd@gmail.com>
2020-09-28 12:08:28 +0100

    Separação das Funções e Adição de Comentários

--- static/buttons.js
+++ static/buttons.js
@@ -5,40 +5,27 @@ class Button {
         this.style = Style
     }
     string(){
+        // Button Structure
         return `<div class=\"col col-lg-2 col-6 div-button\">
             <button class=\"btn ${this.style}\" onclick=\"${this.action}\" type=\"button\">${this.text}</button>
         </div>`
     }
 }
-var buttons = [];
 
+// Buttons Declarations
+var buttons = [];
 buttons.push(new Button("Bloquear", "change('!CM rundll32.exe user32.dll,LockWorkStation');", "btn-success"));
 buttons.push(new Button("Hibernar", "change('!CM rundll32.exe PowrProf.dll,SetSuspendState');", "btn-info"));
 buttons.push(new Button("Encerrar", "change('!CM shutdown -f -s');", "btn-danger"));
 buttons.push(new Button("Reiniciar", "change('!CM shutdown -f -r');", "btn-danger"));
 buttons.push(new Button("IP Config", "change('!CM ipconfig');", "btn-secondary"));
 buttons.push(new Button("WIFI Acessadas", "change('!CM netsh wlan show profiles');", "btn-secondary"));
-buttons.push(new Button("Password do WIFI", "getpro('Nome de Rede:','!CM netsh wlan show profile name=?? key=clear', true);", "btn-dark"));
+buttons.push(new Button("Password do WIFI", "getPrompt('Nome de Rede:','!CM netsh wlan show profile name=?? key=clear', true);", "btn-dark"));
 buttons.push(new Button("Lista de Programas Abertos", "change('!CM tasklist');", "btn-secondary"));
-buttons.push(new Button("Encerrar Programa", "getpro('PID do Programa:','!CM taskkill /PID ?? /f', false);", "btn-dark"));
-buttons.push(new Button("Mostrar Mensagem", "getpro('Mensagem:','!MB ??', false);", "btn-dark"));
-buttons.push(new Button("Digitar Mensagem", "getpro('Mensagem:','!TT ??', false);", "btn-dark"));
-buttons.push(new Button("Combinação de Teclas", "getpro('Combinação (Separador: +):','!TC ??', false);", "btn-dark"));
+buttons.push(new Button("Encerrar Programa", "getPrompt('PID do Programa:','!CM taskkill /PID ?? /f', false);", "btn-dark"));
+buttons.push(new Button("Mostrar Mensagem", "getPrompt('Mensagem:','!MB ??', false);", "btn-dark"));
+buttons.push(new Button("Digitar Mensagem", "getPrompt('Mensagem:','!TT ??', false);", "btn-dark"));
+buttons.push(new Button("Combinação de Teclas", "getPrompt('Combinação (Separador: +):','!TC ??', false);", "btn-dark"));
 
+// Display buttons on Page
 buttons.forEach(button => $("#button-stop").append(button.string()));
-
-function getpro(question, code, aspas) {
-    let text = prompt(question)
-    if (text != null) {
-        if (aspas) {
-            text = '"' + text + '"';
-        }
-        change(code.replace("??", text));
-    }
-}
-
-function change(code) {
-    $('#cmd').val(code);
-}
-
-
---
+++ static/functions.js
@@ -0,0 +1,28 @@
+
+// Get Input using Prompt
+function getPrompt(question, code, aspas) {
+    let text = prompt(question)
+    if (text != null) {
+        if (aspas) {
+            text = '"' + text + '"';
+        }
+        change(code.replace("??", text));
+    }
+}
+
+// Put Instructions Inside InputBox
+function change(code) {
+    let prev = $('#cmd').val();
+    if(prev == ""){
+        $('#cmd').val(code);
+    }else{
+        $('#cmd').val(prev + " && " + code);
+    }
+}
+
+// Clear InputBox
+function clear(){
+    $('#cmd').val("")
+}
+
+
